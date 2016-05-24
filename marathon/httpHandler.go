7de7cbe6ff6972eb92a2eeffdac91a7187cb72a6
jerome DOUCET <jerdct@gmail.com>
2016-05-24 17:09:01 +0200

    some simplification for httpHandler (#13)
    * add a test for sslStore
    
    * fix another test in sslStore_test.go + fix a bug when error on get + unified error handling
    
    * Fix build via build.sh
    
    Had to add prefix github.com/kodokojo/ package to be able to run test
    and build.
    
    * add some test. Start Refactoring of httpHandler
    
    * Relaod only if we have entries in backend
    
    * add a test for sslStore
    
    * do thing simplier in httpHandler

--- marathon/httpHandler.go
+++ marathon/httpHandler.go
@@ -8,19 +8,10 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
 )
 
 type marathonEventHandler struct {
-	projectName          string
 	marathonEventChannel chan commons.MarathonEvent
-	// is it usefull ? this data seems not to be used
-	EventType string
-}
-
-// is it really usefull ? always true, because project name always empty
-func (h *marathonEventHandler) Accept(marathonEvent commons.MarathonEvent) bool {
-	return strings.HasPrefix(marathonEvent.AppId, h.projectName)
 }
 
 func (h *marathonEventHandler) Handle(marathonEvent commons.MarathonEvent) {
@@ -28,27 +19,19 @@ func (h *marathonEventHandler) Handle(marathonEvent commons.MarathonEvent) {
 	h.marathonEventChannel <- marathonEvent
 }
 
-func newMarathonEventHandler(eventType string, marathonEventChannel chan commons.MarathonEvent) *marathonEventHandler {
+func newMarathonEventHandler(marathonEventChannel chan commons.MarathonEvent) *marathonEventHandler {
 	res := new(marathonEventHandler)
-	res.EventType = eventType
 	res.marathonEventChannel = marathonEventChannel
 	return res
 }
 
 type Server struct {
-	configuration         commons.Configuration
-	marathonEventHandlers []*marathonEventHandler
+	configuration        commons.Configuration
+	marathonEventHandler *marathonEventHandler
 }
 
 func NewServer(configuration commons.Configuration, marathonEventChannel chan commons.MarathonEvent) Server {
-
-	marathonEventHandlers := make([]*marathonEventHandler, 4)
-	marathonEventHandlers[0] = newMarathonEventHandler("status_update_event", marathonEventChannel)
-	marathonEventHandlers[1] = newMarathonEventHandler("health_status_changed_event", marathonEventChannel)
-	marathonEventHandlers[2] = newMarathonEventHandler("remove_health_check_event", marathonEventChannel)
-	marathonEventHandlers[3] = newMarathonEventHandler("failed_health_check_event", marathonEventChannel)
-
-	return Server{configuration, marathonEventHandlers}
+	return Server{configuration, newMarathonEventHandler(marathonEventChannel)}
 }
 
 func (s *Server) Start() {
@@ -70,18 +53,10 @@ func (s *Server) Handler(w http.ResponseWriter, r *http.Request) {
 	log.Println(event)
 
 	_, found := utils.GetAppIdMatchKodokojoProjectName(event.AppId)
-	var treated bool = false
 	if found {
-		for i := 0; i < len(s.marathonEventHandlers) && !treated; i++ {
-			handler := s.marathonEventHandlers[i]
-			if handler.Accept(event) {
-				log.Println("Handler", &handler, "handle event", event)
-				treated = true
-				handler.Handle(event)
-			}
-		}
+		log.Println("handle event", event)
+		s.marathonEventHandler.Handle(event)
 	} else {
 		log.Println("Unhandled event", event, ", unable to extract projet name from AppId", event.AppId)
 	}
-
 }
--- marathon/httpHandler_test.go
+++ marathon/httpHandler_test.go
@@ -16,7 +16,7 @@ func Test_Handle(t *testing.T) {
 	// given
 	c := make(chan commons.MarathonEvent, 2)
 	defer close(c)
-	h := newMarathonEventHandler("status_update_event", c)
+	h := newMarathonEventHandler(c)
 	e := commons.MarathonEvent{"status_update_event", "/acme/ci", time.Now(), true, "http://marathon/my-callback"}
 	// when
 	h.Handle(e)
@@ -24,40 +24,13 @@ func Test_Handle(t *testing.T) {
 	assert.Equal(t, e, <-c)
 }
 
-func Test_Accept_true(t *testing.T) {
-	// given
-	c := make(chan commons.MarathonEvent)
-	defer close(c)
-	h := newMarathonEventHandler("status_update_event", c)
-	h.projectName = "/acme"
-	e := commons.MarathonEvent{"status_update_event", "/acme/ci", time.Now(), true, "http://marathon/my-callback"}
-	// when
-	a := h.Accept(e)
-	// then
-	assert.True(t, a)
-}
-
-//todo explain behavior
-
-//func Test_Accept_false(t *testing.T) {
-//	// given
-//	c := make(chan commons.MarathonEvent)
-//	defer close(c)
-//	h := newMarathonEventHandler("status_update_event", c)
-//	e := commons.MarathonEvent{"status_update_event", "/acme/ci", time.Now(), true, "http://marathon/my-callback"}
-//	// when
-//	a := h.Accept(e)
-//	// then
-//	assert.False(t, a)
-//}
-
 func Test_Handler(t *testing.T) {
 	// given
 	e := commons.MarathonEvent{"status_update_event", "/acme/ci", time.Now(), true, "http://marathon/my-callback"}
 	c := make(chan commons.MarathonEvent, 2)
 	defer close(c)
 	s := new(Server)
-	s.marathonEventHandlers = []*marathonEventHandler{newMarathonEventHandler("status_update_event", c)}
+	s.marathonEventHandler = newMarathonEventHandler(c)
 	ts := httptest.NewServer(http.HandlerFunc(s.Handler))
 	b, _ := json.Marshal(e)
 	// when
